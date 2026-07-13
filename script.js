4bd414634f096470199a6d845de52e0551370f17
codeofsayeed <abusayeedemon123@gmail.com>
2026-07-13 20:25:47 +0600

    navbar using html css

---
+++ script.js
@@ -0,0 +1,24 @@
+//Functions with multiple parameters: Ami tumi jabo.\
+{
+  /**
+    function goToPlace(place, time) {
+    console.log(`Ami tumi jabo ${place} at ${time}.`);
+}
+goToPlace("park", "5 PM");
+
+  */
+}
+{
+  /** 
+  function res() {
+  let math = 90;
+  let physics = 87;
+  result = math + physics;
+  console.log(`The adding number of ${math} and ${physics} is=${result}`);
+}
+res();
+
+  */
+}
+
+console.log("dfghfth");
